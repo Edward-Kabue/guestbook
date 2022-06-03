748fd7d5798dfb8c17059661f320d3069ab626f4
Baricho <ekabue0@gmail.com>
2022-06-03 18:30:24 +0300

    create admin section using easyadmin

---
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+
+class ConferenceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Conference::class;
+    }
+
+
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        $fields = parent::configureFields($pageName);
+        $fields[] = AssociationField::new('conference');
+        return $fields;
+    }
+
+}
